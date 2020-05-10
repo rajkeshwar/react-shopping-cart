2a4b0346c143cbea7d661710713a0d1311715f67
rajkeshwar <rajkeshwar.pd@gmail.com>
2020-05-10 11:43:21 +0200

    push to gh-pages

---
+++ precache-manifest.95132256c08feafc713e1354a968daae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f147f8bd56e04b770f4d13e3c5c3bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c45c8b4d776fd12c17",
+    "url": "/static/css/main.bff4a5a8.chunk.css"
+  },
+  {
+    "revision": "378e97b247edfbf1a6f0",
+    "url": "/static/js/2.17436aac.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.17436aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c45c8b4d776fd12c17",
+    "url": "/static/js/main.6695ccf9.chunk.js"
+  },
+  {
+    "revision": "98fe19c65c81cb61ef7e",
+    "url": "/static/js/runtime-main.54820a21.js"
+  }
+]);
